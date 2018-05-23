d7cfb23064ff39e4fd6f7c4d04ab90d9d9634a4a
Tobiloba Adejumo <eniolaplus08@gmail.com>
2018-05-23 12:18:33 +0100

    S02.02-Creating Menus

--- S02.02-Exercise-Menus/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S02.02-Exercise-Menus/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -18,6 +18,8 @@ package com.example.android.sunshine;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.example.android.sunshine.data.SunshinePreferences;
@@ -102,6 +104,27 @@ public class MainActivity extends AppCompatActivity {
     // TODO (4) Set the title of the menu item to "Refresh" using strings.xml
 
     // TODO (5) Override onCreateOptionsMenu to inflate the menu for this Activity
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.forecast, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_refresh) {
+            mWeatherTextView.setText("");
+            loadWeatherData();
+
+            return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
     // TODO (6) Return true to display the menu
 
     // TODO (7) Override onOptionsItemSelected to handle clicks on the refresh button
