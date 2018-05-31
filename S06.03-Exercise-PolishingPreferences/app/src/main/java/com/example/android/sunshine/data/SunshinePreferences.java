a64fb8df4b4fd835c0f77b26ddfad8ac14e02ba6
Tobiloba Adejumo <eniolaplus08@gmail.com>
2018-05-31 23:11:29 +0100

    S06.03-Polishing preferences

--- S06.03-Exercise-PolishingPreferences/app/src/main/java/com/example/android/sunshine/data/SunshinePreferences.java
+++ S06.03-Exercise-PolishingPreferences/app/src/main/java/com/example/android/sunshine/data/SunshinePreferences.java
@@ -16,6 +16,10 @@
 package com.example.android.sunshine.data;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.example.android.sunshine.R;
 
 public class SunshinePreferences {
 
@@ -89,7 +93,11 @@ public class SunshinePreferences {
     public static String getPreferredWeatherLocation(Context context) {
         // TODO (1) Return the user's preferred location
         /** This will be implemented in a future lesson **/
-        return getDefaultWeatherLocation();
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        String locationOfKey = context.getString(R.string.pref_location_key);
+        String locationDefault = context.getString(R.string.pref_location_default);
+        String preferredLocation = sharedPreferences.getString(locationOfKey,locationDefault);
+        return preferredLocation;
     }
 
     /**
@@ -102,7 +110,19 @@ public class SunshinePreferences {
     public static boolean isMetric(Context context) {
         // TODO (2) Return true if the user's preference for units is metric, false otherwise
         /** This will be implemented in a future lesson **/
-        return true;
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        String unitsKey = context.getString(R.string.pref_units_key);
+        String defaultUnits = context.getString(R.string.pref_units_metric);
+        String preferredUnits = sharedPreferences.getString(unitsKey,defaultUnits);
+        String unitMetric = context.getString(R.string.pref_units_metric);
+        boolean usersPreference;
+        if (unitMetric.equals(preferredUnits)) {
+                usersPreference = true;
+        } else {
+            usersPreference = false;
+        }
+
+        return usersPreference;
     }
 
     /**
